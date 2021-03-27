db2a92df93a4858865e0575dadf3a91e79e9a136
dytroInc <68156872+dytroInc@users.noreply.github.com>
2021-03-27 14:44:47 +0900

    Changes in SamplePlugin.kt

---
+++ src/main/kotlin/com/github/dytroInc/sample/plugin/SamplePlugin.kt
@@ -0,0 +1,10 @@
+package com.github.monun.sample.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * @author Dytro
+ *
+ * Forked from monun/tap-sample-plugin
+ */
+class SamplePlugin : JavaPlugin()
