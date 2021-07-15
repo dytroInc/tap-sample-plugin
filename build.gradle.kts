08aead7090a841e56e9bade5eb58df566fbf74a2
dytroInc <68156872+dytroInc@users.noreply.github.com>
2021-07-15 15:34:33 +0900

    Delete old spigot

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
     testImplementation("org.mockito:mockito-core:3.6.28")
-    testImplementation("org.spigotmc:spigot:1.17-R0.1-SNAPSHOT")
     testImplementation("org.spigotmc:spigot-api:1.17-R0.1-SNAPSHOT")
 }
 
